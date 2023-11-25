b1daf7348a54ab91384b1786cde4301b5f473742
SREEKANTH <98770751+SREE1949@users.noreply.github.com>
2023-11-25 17:42:31 +0530

    Update print_line.py

--- text_file_ops/print_line.py
+++ text_file_ops/print_line.py
@@ -1,1 +1,11 @@
+# this code is to print the line from text file given by the user
 
+file_name=input("Enter the file name: ")
+n=input("Enter the line number: ")
+
+f=open(file_name,"rt")
+m=int(n)-1
+for x in range(m) :
+ f.readline()
+print(f.readline())
+f.close()                    
